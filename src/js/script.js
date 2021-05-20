509b234ecf913d22476154d4b3108ce7c24b9e11
Jakub Jurga <>
2021-05-20 15:04:36 +0100

    Add form and modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,51 @@
 /* global Chart */
 
 function toggleMenu(visible) {
-    document.querySelector('.menu').classList.toggle('show')
-  };
-  document.querySelector('.hamburger').addEventListener('click', function(e) {
-    e.preventDefault();
-    toggleMenu()
-  });
+  document.querySelector('.menu').classList.toggle('show', visible);
+}
+document.querySelector('.hamburger').addEventListener('click', function(e) {
+  e.preventDefault();
+  toggleMenu();
+});
 
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+document.querySelector('#overlay').addEventListener('click', function (e) {
+  if (e.target === this) {
+    closeModal();
+  }
+});
+
+document.addEventListener('keyup', function (e) {
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+});
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function (modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
+
+document.querySelector('.quit').addEventListener('click', function (e) {
+  openModal('#myModalQuit');
+});
+
+document.querySelector('.top-bar-login').addEventListener('click', function (e) {
+  openModal('#myModalLogin');
+});
 
 var chart1 = document.getElementById('myChart').getContext('2d');
 
